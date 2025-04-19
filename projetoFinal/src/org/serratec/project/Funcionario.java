6220a64214f238cfb4f2054646f55d163a8c85d5
wolf011 <duckhouse_bousoukis@aleeas.com>
2025-04-18 21:07:12 -0300

    teste

--- projetoFinal/src/org/serratec/project/Funcionario.java
+++ projetoFinal/src/org/serratec/project/Funcionario.java
@@ -4,4 +4,15 @@ public class Funcionario extends Pessoa {
 	private Double salarioBruto;
 	private Double descontoInss;
 	private Double descontoIr;
+	
+	
+	public Funcionario(Double salarioBruto, Double descontoInss, Double descontoIr) {
+		super();
+		this.salarioBruto = salarioBruto;
+		this.descontoInss = descontoInss;
+		this.descontoIr = descontoIr;
+	}
+	
+	
+	
 }
