212550807c86e947d3f214cccba38254755516cf
joaovsc0de <ayahkoskaiba@gmail.com>
2025-04-20 18:43:39 -0300

    finalizacao funcionario DAO. Adicao configuracao envs

--- projetoFinal/src/org/serratec/dbconnection/ConnectionFac.java
+++ projetoFinal/src/org/serratec/dbconnection/ConnectionFac.java
@@ -3,25 +3,24 @@ package org.serratec.dbconnection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 import java.util.Scanner;
+
+import org.DotEnvLoader;
+
 
 public class ConnectionFac {
     private String url = "jdbc:postgresql://localhost:5432/salariomanager_db";
-
+    private Connection connection = null;
     public Connection getConnection() {
-        String username = "postgres";
-        String password = "123456";
-        Connection connection = null;
-
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Usuário:");
-//        username = sc.nextLine();
-//        System.out.println("Senha:");
-//        password = sc.nextLine();
+    	String filePath="C:/Users/ayahk/Desktop/New folder/projetoPOO/projetoFinal/src/org/.env";
+        Map<String, String> envs = DotEnvLoader.loadEnv(System.getProperty("user.dir")+"/src/org/.env");
+        String username = envs.get("username");
+        String password = envs.get("password");
         
         try {
             System.out.println("Fazendo conexão...");
-            connection = DriverManager.getConnection(url, username, password);
+            this.connection = DriverManager.getConnection(url, username, password);
             System.out.println("Conectado com sucesso!");
         } catch (SQLException e) {
             System.err.println("Conexão não efetuada!");
@@ -29,5 +28,14 @@ public class ConnectionFac {
             return null;
         }
         return connection;
+    }
+    
+    public void closeConnection() {
+    	try {
+    		this.connection.close();
+    		System.out.println("Conexão fechada.");
+		} catch (SQLException e) {
+			System.out.println("Erro ao fechar conexão."+e.getMessage());
+		}
     }
 }
